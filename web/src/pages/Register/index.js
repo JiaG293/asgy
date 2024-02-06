7517395946cdf3f32d04e40d30d4193876394e54
SangHynh <huynhvansang.020102@gmail.com>
2024-02-06 13:34:20 +0700

    [Add]: handleRegister from Register

--- web/src/pages/Register/index.js
+++ web/src/pages/Register/index.js
@@ -1,84 +1,127 @@
-import { FaArrowAltCircleRight as NextButton } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
-import "../Register/Register.scss";
+import { BiSolidUserRectangle as UserIcon } from "react-icons/bi";
+import { IoMdLock as PasswordIcon } from "react-icons/io";
+import { MdEmail as EmailIcon } from "react-icons/md";
+import { IoIosSend as SendIcon } from "react-icons/io";
+import { MdOutlineVerifiedUser as OTPIcon } from "react-icons/md";
+import axios from "axios";
+
+import "./Register.scss";
+import { useState } from "react";
 
 // giao diện register
 function Register() {
-  // hàm chuyển hướng trang sang trang tiếp
   const navigate = useNavigate();
-  const navigateToNext = () => {
-    navigate("/register-next");
+  const [getUsername, setUsername] = useState("");
+  const [getEmail, setEmail] = useState("");
+  const [getPassword, setPassword] = useState("");
+  const [getRepassword, setRepassword] = useState("");
+
+  // hàm chuyển hướng trang sang trang tiếp
+  const navigateToCreateUserInfor = () => {
+    // navigate("/create-user-infor");
   };
-//render
+
+  //hàm đăng ký
+  const handleRegister = async () => {
+    try {
+      //1. lấy value trên form
+      const username = getUsername;
+      const email = getEmail;
+      const password = getPassword;
+      await axios.post("http://localhost:5000/v1/signup", {
+        username: username,
+        email: email,
+        password: password,
+      });
+    } catch (error) {}
+  };
+
+  //render
   return (
     <div className="register-container">
       <form className="register-form" action="">
-        <h1>Hồ sơ người dùng</h1>
-        {/* Tên người dùng*/}
+        <h1>Thông tin tài khoản</h1>
+        {/* Tên tài khoản */}
         <div className="register-input-box">
-          <label>Họ tên</label>
           <input
             type="text"
             className="register-input"
-            placeholder="Nhập họ và tên"
+            placeholder="Tên tài khoản"
+            value={getUsername}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
           ></input>
+          <UserIcon className="register-icon"></UserIcon>
         </div>
-        {/* Giới tính */}
+        {/*Email*/}
         <div className="register-input-box">
-          <label>Giới tính</label>
-          <div className="register-radio">
-            <input
-              type="radio"
-              className="register-radio"
-              name="gender"
-            ></input>
-            <p>Nam</p>
-          </div>
-          <div className="register-radio">
-            <input
-              type="radio"
-              className="register-radio"
-              name="gender"
-            ></input>
-            <p>Nữ</p>
-          </div>
-          <div className="register-radio">
-            <input
-              type="radio"
-              className="register-radio"
-              name="gender"
-            ></input>
-            <p>Bí mật</p>
-          </div>
-        </div>
-        {/* Ngày sinh */}
-        <div className="register-input-box">
-          <label>Ngày sinh</label>
-          <input type="date" className="register-input"></input>
-        </div>
-        {/* Số điện thoại */}
-        <div className="register-input-box">
-          <label>SĐT</label>
           <input
             type="text"
             className="register-input"
-            placeholder="Nhập số điện thoại"
+            placeholder="Nhập email"
+            value={getEmail}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           ></input>
+          <EmailIcon className="register-icon"></EmailIcon>
         </div>
-        {/* Địa chỉ */}
+        {/*OTP*/}
+        <div className="register-OTP-div">
+          <div className={"register-input-box  register-OTP"}>
+            <input
+              type="text"
+              className="register-input"
+              placeholder="Nhập OTP"
+            ></input>
+            <OTPIcon className="register-icon"></OTPIcon>
+          </div>
+          <button className="register-btn-send-OTP">
+            <span style={{ fontSize: 9 }}>Gửi OTP</span>
+            <SendIcon></SendIcon>
+          </button>
+        </div>
+        {/* Mật khẩu */}
         <div className="register-input-box">
-          <label>Địa chỉ</label>
           <input
-            type="text"
+            type="password"
             className="register-input"
-            placeholder="Nhập địa chỉ"
+            placeholder="Nhập mật khẩu"
+            value={getPassword}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
           ></input>
+          <PasswordIcon className="register-icon"></PasswordIcon>
+        </div>
+        {/* Mật khẩu nhập lại*/}
+        <div className="register-input-box">
+          <input
+            type="password"
+            className="register-input"
+            placeholder="Nhập lại mật khẩu"
+            value={getRepassword}
+            onChange={(e) => {
+              setRepassword(e.target.value);
+            }}
+          ></input>
+          <PasswordIcon className="register-icon"></PasswordIcon>
+        </div>
+        {/* Chính sách */}
+        <div className="register-policy">
+          <input type="checkbox" className="register-checkbox"></input>
+          <p>
+            Tôi đã đồng ý với <a href="">Điều khoản</a>,{" "}
+            <a href="">Chính sách quyền riêng tư</a> và{" "}
+            <a href="">Chính sách Cookies</a>
+          </p>
         </div>
         {/* Button đăng ký */}
         <div className="register-button-box">
-          <button type="submit" onClick={navigateToNext}>
-            <span>Bước tiếp theo</span>
-            <NextButton></NextButton>
+          <button type="button" onClick={handleRegister}>
+            <span>Đăng ký</span>
           </button>
         </div>
         {/* Link đăng nhập */}
--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -3,14 +3,12 @@ import Home from '../pages/Home'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
 import ForgotPassword from '../pages/ForgotPassword'
-import RegisterNext from '../pages/RegisterNext'
 
 // khong can dang nhap van xem duoc
 const publicRoutes = [
     {path:'/home', component:Home},
     {path:'/login', component:Login},
     {path:'/register', component:Register},
-    {path:'/register-next', component:RegisterNext},
     {path:'/forgot-password', component:ForgotPassword},
 ]
 
