031798a99c6084df800331aaf26cfaeac220fc26
JiaG293 <76744254+JiaG293@users.noreply.github.com>
2024-03-18 22:35:00 +0700

    Update index.js

--- server/src/routes/chats/index.js
+++ server/src/routes/chats/index.js
@@ -10,14 +10,14 @@ const path = require('path');
 
 
 router.route('/chat-demo').get((req, res, next) => {
-    res.sendFile(path.join(__dirname + '\\index4.html'))
+    res.sendFile(path.join(__dirname + '/index4.html'))
 });
 
 router.route('/chat').get((req, res, next) => {
-    res.sendFile(path.join(__dirname + '\\index5.html'))
+    res.sendFile(path.join(__dirname + '/index5.html'))
 });
 router.route('/home').get((req, res, next) => {
-    res.sendFile(path.join(__dirname + '\\home.html'))
+    res.sendFile(path.join(__dirname + '/home.html'))
 });
 
 //Luon luon su dung router duoi authentication
