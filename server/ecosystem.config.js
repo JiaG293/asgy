7b07dbb55726755e9c59dc8b170a5572eed99d3f
JiaG <giau32002@gmail.com>
2024-05-25 21:26:24 +0700

    done

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,16 +1,22 @@
 module.exports = {
-    apps: [{
-        name: "server-asgy",
-        script: "./server.js",
-        exec_mode: 'cluster',
-        instances: 4,
-        watch: true,
-        ignore_watch: ["node_modules"],
-        env_production: {
-            NODE_ENV: "production"
-        },
-        env_development: {
-            NODE_ENV: "development"
-        }
-    }]
-}
+  apps : [{
+    script: 'server.js',
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
